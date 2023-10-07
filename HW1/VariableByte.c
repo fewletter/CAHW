1fb1afa86059bcc1c88ebf273cc032d8447b73c6
fewletter <fewletter@gmail.com>
2023-10-08 02:58:27 +0800

    Change the array type to fit RISC-V register

--- HW1/VariableByte.c
+++ HW1/VariableByte.c
@@ -22,38 +22,49 @@ uint16_t count_leading_zeros(uint64_t x)
     return (64 - (x & 0x7f));
 }
 
-void encodeVariableByte(uint64_t value, uint8_t *encodebytes, int len) {
-    for (int i = 0; i < len; i++)
-        encodebytes[i] = i == 0 ? ((value >> (i * 7)) & 0x7F) & 0x7f : ((value >> (i * 7)) & 0x7F) | 0x80;
+void encodeVariableByte(uint64_t value, uint32_t *encodebytes, int len) {
+    uint32_t bitmask1 = 0x7f;
+    uint32_t bitmask2 = 0x80;
+    for (int i = 0; i < len; i++) {
+        if (i % 4 == 0) {
+            bitmask1 = 0x7f;
+            bitmask2 = 0x80;
+        }
+        encodebytes[i/4] |= i/4 == 0 ? (value & bitmask1) << i : (((value >> 32) & bitmask1) << (i % 4));
+        encodebytes[i/4] = i == 0 ? encodebytes[i/4] : (encodebytes[i/4] | bitmask2);
+        bitmask1 <<= 7;
+        bitmask2 <<= 8;
+    }
 }
 
 int main() {
-    uint64_t testdata[3] = { 128, 0xffffffffffffff, 0xffffffffffffffff}; /*128, 72057594037927935, 18446744073709551615*/
+    uint64_t testdata[3] = { 128, 0xfffffff, 0xfffffffffffffff};
     int len1 = (63 - count_leading_zeros(testdata[0])) / 7 + 1;
-    uint8_t encodedData1[len1];
+    printf("%d\n", len1);
+    uint32_t encodedData1[1] = {0}; 
 
     int len2 = (63 - count_leading_zeros(testdata[1])) / 7 + 1;
-    uint8_t encodedData2[len2];
+    uint32_t encodedData2[1] = {0};
     
     int len3 = (63 - count_leading_zeros(testdata[2])) / 7 + 1;
-    uint8_t encodedData3[len3];
+    uint32_t encodedData3[2] = {0, 0};
 
     encodeVariableByte(testdata[0], encodedData1, len1);
     encodeVariableByte(testdata[1], encodedData2, len2);
     encodeVariableByte(testdata[2], encodedData3, len3);
 
     printf("Encoded Bytes: ");
-    for (int i = 0; i < len1; ++i) {
+    for (int i = 0; i < 1; ++i) {
         printf("%x ", encodedData1[i]);
     }
     printf("\n");
     printf("Encoded Bytes: ");
-    for (int i = 0; i < len2; ++i) {
+    for (int i = 0; i < 1; ++i) {
         printf("%x ", encodedData2[i]);
     }
     printf("\n");
     printf("Encoded Bytes: ");
-    for (int i = 0; i < len3; ++i) {
+    for (int i = 0; i < 2; ++i) {
         printf("%x ", encodedData3[i]);
     }
     printf("\n");
